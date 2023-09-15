bf2342d2505a549c20dc219ef5d3856906d6f687
SORN Sea <sorn.sea@gmail.com>
2023-09-15 13:50:44 +0700

    added send email verification

--- src/main/java/recordDailySpending/web/RegisterServlet.java
+++ src/main/java/recordDailySpending/web/RegisterServlet.java
@@ -3,6 +3,7 @@ package recordDailySpending.web;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,35 +12,49 @@ import javax.servlet.http.HttpServletResponse;
 
 import recordDailySpending.dao.PersonDao;
 import recordDailySpending.model.Person;
+import recordDailySpending.utils.EmailUtility;
 
 @WebServlet("/registerServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private PersonDao personDao;
 
+	private String host;
+	private String port;
+	private String user;
+	private String pass;
+
 	public void init() {
 		personDao = new PersonDao();
+
+		// reads SMTP server setting from web.xml file
+		ServletContext context = getServletContext();
+		host = context.getInitParameter("host");
+		port = context.getInitParameter("port");
+		user = context.getInitParameter("user");
+		pass = context.getInitParameter("pass");
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+
 		register(request, response);
-	
+		sendEmail(request, response);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-	
-	private void register(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+	private void register(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
 		String email = request.getParameter("email");
 		String phone = request.getParameter("phone");
 		String password = request.getParameter("password");
-
 
 		Person person = new Person();
 		person.setName(name);
@@ -48,53 +63,41 @@ public class RegisterServlet extends HttpServlet {
 		person.setPhone(phone);
 		person.setPassword(password);
 
-//		try {
-//			personDao.registerPerson(person);
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//		response.sendRedirect("login.jsp");
-		
 		try {
 			int result = personDao.registerPerson(person);
-			if(result == 1) {
+			if (result == 1) {
 				request.setAttribute("NOTIFICATION", "User Registered Successfully!");
 			}
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		RequestDispatcher dispatcher = request.getRequestDispatcher("register/register.jsp");
-		dispatcher.forward(request, response);
-		
-//		String firstName = request.getParameter("firstName");
-//		String lastName = request.getParameter("lastName");
-//		String username = request.getParameter("username");
-//		String password = request.getParameter("password");
-//
-//		User employee = new User();
-//		employee.setFirstName(firstName);
-//		employee.setLastName(lastName);
-//		employee.setUsername(username);
-//		employee.setPassword(password);
-//
-//		try {
-//			int result = userDao.registerEmployee(employee);
-//			if(result == 1) {
-//				request.setAttribute("NOTIFICATION", "User Registered Successfully!");
-//			}
-//			
-//		} catch (Exception e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
 //		RequestDispatcher dispatcher = request.getRequestDispatcher("register/register.jsp");
 //		dispatcher.forward(request, response);
+	}
+
+	private void sendEmail(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		// reads form fields
+		String recipient = "phengdavid979@gmail.com";
+		String subject = "Registeration verification";
+		String content = "Pls fill this code: 554876";
+
+		String resultMessage = "";
+
+		try {
+			EmailUtility.sendEmail(host, port, user, pass, recipient, subject, content);
+			resultMessage = "The e-mail was sent successfully";
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			resultMessage = "There were an error: " + ex.getMessage();
+		} finally {
+			request.setAttribute("Message", resultMessage);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("register/verification.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
